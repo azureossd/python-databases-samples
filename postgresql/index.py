31d3dae6a7c8362e0f6f4481aab4f42a90990b28
Edison Garcia Chinas <edisga@microsoft.com>
2020-06-13 21:07:15 -0500

    Adding Postgresql

---
+++ postgresql/index.py
@@ -0,0 +1,94 @@
+import psycopg2
+import sys, os
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+cnx  = None
+cursor = None
+
+messages = []
+
+def Connect():
+    global cnx 
+    conn_string="host='{}' dbname='{}' user='{}' password='{}'".format(os.environ.get('HOST'), os.environ.get('DATABASE'),os.environ.get('USER'),os.environ.get('PASSWORD'))
+    print(conn_string)
+    cnx  = psycopg2.connect(conn_string)
+    global cursor
+    cursor = cnx.cursor()
+    messages.append("Connecting to Database")
+
+def CreateTable():
+    #Check if table exists
+    DeleteTable()
+    messages.append("Dropping existing table")
+
+    cursor.execute("""
+        CREATE TABLE Users 
+        (id serial PRIMARY KEY, 
+        name VARCHAR(50) NOT NULL, 
+        lastname VARCHAR(50) NOT NULL);
+        """)
+
+    cnx.commit()
+    messages.append("Creating Table Users")
+
+def QueryRow():
+    cursor.execute("SELECT * FROM Users where id=1") 
+    row = cursor.fetchone() 
+    messages.append("Querying from Table and selecting 1")
+    while row: 
+        print(row[0])
+        row = cursor.fetchone()
+
+def QueryAllRows():
+    cursor.execute("SELECT * FROM Users") 
+    rows = cursor.fetchall()
+    messages.append("Querying all rows from Table")
+    for row in rows:
+        print(row, end='\n')
+    
+def InsertRow():
+    cursor.execute("""
+    INSERT INTO Users (name, lastname)
+    VALUES (%s, %s)""", ('Name','LastName')) 
+    cnx.commit()
+    messages.append("Inserting row")
+
+def DeleteRow():
+    cursor.execute("""
+    DELETE FROM Users where id=1""")  
+    cnx.commit()
+    messages.append("Deleting row")
+
+def DeleteTable():
+    cursor.execute("""
+    DROP TABLE IF EXISTS Users 
+    """)
+    cnx.commit()
+    messages.append("Dropping Table Users")
+
+@app.route('/')
+def home():
+    try:
+        Connect()
+        CreateTable()
+        InsertRow()
+        QueryRow()
+        QueryAllRows()
+        DeleteRow()
+        DeleteTable()
+    except Exception as ex:
+        print("Raised exception caught: ", ex.args)
+    finally:
+        cursor.close()
+        cnx.close()
+        messages.append("Closing connection to Database")
+
+    return jsonify(messages)
+
+if __name__ == '__main__':
+    app.run()
+
+
+
